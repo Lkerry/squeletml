6e9ada3af91c911f8ff518889dcc862640bca6b8
Jean-Philippe Fleury <contact@jpfleury.net>
2009-05-06 22:27:48 -0400

    Ajout d'un script de génération automatique d'un fichier de config pour une galerie

---
+++ admin/galeries.php
@@ -0,0 +1,54 @@
+<?php
+$baliseTitle = "Gestion des galeries";
+include 'inc/premier.inc.php';
+
+include '../init.inc.php';
+
+if (isset($_POST['soumettre']))
+{
+	$cheminGalerie = $racine . '/site/fichiers/galeries/' . $_POST['id'] . '/';
+	
+	if (!file_exists($cheminGalerie))
+	{
+		echo "<p class='erreur'>La galerie {$_POST['id']} n'existe pas.</p>";
+	}
+	else
+	{
+		$fic = opendir($cheminGalerie) or die("<p class='erreur'>Erreur lors de l'ouverture du dossier $cheminGalerie.</p>");
+		
+		$listeFichiers = '';
+		
+		while($fichier = @readdir($fic))
+		{
+			if(!is_dir($cheminGalerie . '/' . $fichier) && $fichier != '.' && $fichier != '..')
+			{
+				if ($_POST['exclusion'] != 'vignette' || !preg_match('/-vignette\.[[:alpha:]]{3,4}$/', $fichier))
+				{
+					$listeFichiers .= "grandeNom=$fichier\n__IMG__\n";
+				}
+			}
+		}
+		
+		closedir($fic);
+		
+		echo '<h2>Résultat</h2>' . "\n" . '<textarea name="listeFichiers" readonly="readonly">' . $listeFichiers . '</textarea>' . "\n";
+	}
+}
+?>
+
+<h1>Gestion des galeries</h1>
+
+<h2>Générer un fichier de configuration de base d'une galerie</h2>
+
+<p>Pour générer automatiquement la liste des images d'une galerie, et ce sous la forme <code>grandeNom=grandeImage.extension</code>, remplissez le formulaire ci-dessous. Optionnellement, vous pouvez exclure des images du résultat.</p>
+
+<form action="<? echo $action; ?>" method="post">
+<div>
+<p><label>Id de la galerie:</label><br />
+<input type="text" name="id" /><br />
+<input type=checkbox name="exclusion" value="vignette" checked="checked" /> <label>Ne pas tenir compte des fichiers terminant par <code>-vignette.extension</code></label></p>
+<p><input type="submit" name="soumettre" value="Soumettre" /></p>
+</div>
+</form>
+
+<?php include 'inc/dernier.inc.php'; ?>
--- inc/galerie.inc.php
+++ inc/galerie.inc.php
@@ -10,56 +10,7 @@ foreach (init($racine, langue($langue)) as $fichier)
 phpGettext($racine, langue($langue));
 
 // Insertion du tableau contenant la liste des oeuvres à afficher
-// Recherche `site/inc/galerie-$idGalerie.inc.php`, sinon inclut `inc/galerie-0.inc.php`
-/*
-Chaque oeuvre de la galerie possède sa propre entrée dans le tableau $galerie. Voici la structure d'une entrée:
-
- id: id image. L'id est utilisé dans l'URL au lieu de l'indice de l'oeuvre dans le tableau pour identifier l'oeuvre, ce qui permet de facilement déplacer des oeuvres dans la galerie sans modifier leur URL. Si vide, l'id sera généré automatiquement à partir du nom de fichier image.
-
- vignetteNom: nom Vignette. Si vide, correspond à grandeNom(sans extension)-vignette.extension
-
- vignetteLargeur: largeur Vignette. Si vignetteLargeur ou vignetteHauteur sont renseignées, seulement la ou les informations renseignées seront affichées. Si les deux sont vides, les attributs `width` et `height` seront calculés automatiquement.
-
- vignetteHauteur: hauteur Vignette. Voir vignetteLargeur.
-
- vignetteAlt: alt Vignette. Si vide, génération automatique.
-
- grandeNom: nom Grande. Champ obligatoire.
-
- grandeLargeur: largeur Grande. Voir vignetteLargeur.
-
- grandeHauteur: hauteur Grande. Voir grandeLargeur.
-
- grandeAlt: alt Grande. Si vide, génération automatique.
-
- grandeCommentaire: commentaire Grande. Message affiché sous l'image.
-
- pageGrandeTitle: title page Grande ; laisser vide pour génération automatique.
-
- pageGrandeDescription: description page Grande ; laisser vide pour génération automatique.
-
- pageGrandeKeywords: keywords page Grande ; laisser vide pour génération automatique.
-
-Voici une entrée vide qu'il est possible de copier/coller:
-
-array (
-	'id'                     => "",
-	'vignetteNom'            => "",
-	'vignetteLargeur'        => "",
-	'vignetteHauteur'        => "",
-	'vignetteAlt'            => "",
-	'grandeNom'              => "*",
-	'grandeLargeur'          => "",
-	'grandeHauteur'          => "",
-	'grandeAlt'              => "",
-	'grandeCommentaire'      => "",
-	'pageGrandeTitle'        => "",
-	'pageGrandeDescription'  => "",
-	'pageGrandeKeywords'     => "",
-),
-
-*/
-
+// Recherche `site/inc/galerie-$idGalerie.txt`, sinon inclut `inc/galerie-0.txt`
 if (isset($idGalerie)
 	&& file_exists($racine . '/site/fichiers/galeries/' . $idGalerie . '/')
 	&& file_exists($racine . '/site/inc/galerie-' . $idGalerie . '.txt'))
