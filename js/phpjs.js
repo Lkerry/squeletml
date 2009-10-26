2e1c7175a3a1ba9fed1caa7ef33ac7c7ffea5d62
Jean-Philippe Fleury <contact@jpfleury.net>
2009-10-26 13:44:51 -0400

    - Ajout de *Table of Contents jQuery Plugin*, sous licence MIT. Voir <http://fuelyourcoding.com/table-of-contents-jquery-plugin/>.
    - Ajout de fonctions PHPJS.

--- js/phpjs.js
+++ js/phpjs.js
@@ -310,3 +310,187 @@ function html_entity_decode (string, quote_style) {
     return tmp_str;
 }
 
+function i18n_loc_get_default () {
+    // !No description available for i18n_loc_get_default. @php.js developers: Please update the function summary text file.
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/i18n_loc_get_default
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // %          note 1: Renamed in PHP6 from locale_get_default(). Not listed yet at php.net
+    // %          note 2: List of locales at http://demo.icu-project.org/icu-bin/locexp
+    // %          note 3: To be usable with sort() if it is passed the SORT_LOCALE_STRING sorting flag: http://php.net/manual/en/function.sort.php
+    // -    depends on: i18n_loc_set_default
+    // *     example 1: i18n_loc_get_default();
+    // *     returns 1: 'en_US_POSIX'
+
+    // BEGIN REDUNDANT
+    this.php_js = this.php_js || {};
+    // END REDUNDANT
+    return this.php_js.i18nLocale || (i18n_loc_set_default('en_US_POSIX'), 'en_US_POSIX'); // Ensure defaults are set up
+}
+
+function i18n_loc_set_default (name) {
+    // !No description available for i18n_loc_set_default. @php.js developers: Please update the function summary text file.
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/i18n_loc_set_default
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // %          note 1: Renamed in PHP6 from locale_set_default(). Not listed yet at php.net
+    // %          note 2: List of locales at http://demo.icu-project.org/icu-bin/locexp (use for implementing other locales here)
+    // %          note 3: To be usable with sort() if it is passed the SORT_LOCALE_STRING sorting flag: http://php.net/manual/en/function.sort.php
+    // *     example 1: i18n_loc_set_default('pt_PT');
+    // *     returns 1: true
+
+    // BEGIN REDUNDANT
+    this.php_js = this.php_js || {};
+    // END REDUNDANT
+
+    this.php_js.i18nLocales = {
+        en_US_POSIX : {
+            sorting :
+                function ( str1, str2 ) { // Fix: This one taken from strcmp, but need for other locales; we don't use localeCompare since its locale is not settable
+                    return ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 );
+                }
+        }
+    };
+
+    this.php_js.i18nLocale = name;
+    return true;
+}
+
+function krsort (array, sort_flags) {
+    // Sort an array by key value in reverse order  
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/krsort
+    // +   original by: GeekFG (http://geekfg.blogspot.com)
+    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // %          note: The examples are correct, this is a new way
+    // -    depends on: i18n_loc_get_default
+    // *     example 1: data = {2: 'van', 3: 'Zonneveld', 1: 'Kevin'};
+    // *     example 1: krsort(data);
+    // *     results 1: data == {3: 'Kevin', 2: 'van', 1: 'Zonneveld'}
+    // *     returns 1: true
+    var tmp_arr={}, keys=[], sorter, i, key, that=this;
+
+    switch (sort_flags) {
+        case 'SORT_STRING': // compare items as strings
+            sorter = function (a, b) {
+                return that.strnatcmp(b, a);
+            };
+            break;
+        case 'SORT_LOCALE_STRING': // compare items as strings, based on the current locale (set with  i18n_loc_set_default() as of PHP6)
+            var loc = this.i18n_loc_get_default();
+            sorter = this.php_js.i18nLocales[loc].sorting;
+            break;
+        case 'SORT_NUMERIC': // compare items numerically
+            sorter = function (a, b) {
+                return (b - a);
+            };
+            break;
+        case 'SORT_REGULAR': // compare items normally (don't change types)
+        default:
+            sorter = function (a, b) {
+                if (a < b) {
+                    return 1;
+                }
+                if (a > b) {
+                    return -1;
+                }
+                return 0;
+            };
+            break;
+    }
+
+    // Make a list of key names
+    for (key in array) {
+        keys.push(key);
+    }
+
+    keys.sort(sorter);
+
+    // Rebuild array with sorted key names
+    for (i = 0; i < keys.length; i++) {
+        key = keys[i];
+        tmp_arr[key] = array[key];
+        delete array[key];
+    }
+    for (i in tmp_arr) {
+        array[i] = tmp_arr[i];
+    }
+
+    return true;
+}
+
+function strtr (str, from, to) {
+    // Translates characters in str using given translation tables  
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/strtr
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // +      input by: uestla
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Alan C
+    // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +      input by: Taras Bogach
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // -   depends on: krsort
+    // *     example 1: $trans = {'hello' : 'hi', 'hi' : 'hello'};
+    // *     example 1: strtr('hi all, I said hello', $trans)
+    // *     returns 1: 'hello all, I said hi'
+    // *     example 2: strtr('äaabaåccasdeöoo', 'äåö','aao');
+    // *     returns 2: 'aaabaaccasdeooo'
+    // *     example 3: strtr('ääääääää', 'ä', 'a');
+    // *     returns 3: 'aaaaaaaa'
+    // *     example 4: strtr('http', 'pthxyz','xyzpth');
+    // *     returns 4: 'zyyx'
+    // *     example 5: strtr('zyyx', 'pthxyz','xyzpth');
+    // *     returns 5: 'http'
+    // *     example 6: strtr('aa', {'a':1,'aa':2});
+    // *     returns 6: '2'
+    var fr = '', i = 0, j = 0, lenStr = 0, lenFrom = 0;
+    var tmpFrom = [];
+    var tmpTo   = [];
+    var ret = '';
+    var match = false;
+
+    // Received replace_pairs?
+    // Convert to normal from->to chars
+    if (typeof from === 'object') {
+        this.krsort(from);
+        for (fr in from) {
+            tmpFrom.push(fr);
+            tmpTo.push(from[fr]);
+        }
+
+        from = tmpFrom;
+        to   = tmpTo;
+    }
+    
+    // Walk through subject and replace chars when needed
+    lenStr  = str.length;
+    lenFrom = from.length;
+    for (i = 0; i < lenStr; i++) {
+        match = false;
+        for (j = 0; j < lenFrom; j++) {
+            if (str.substr(i, from[j].length) == from[j]) {
+                match = true;
+
+                // Fast forward
+                i = (i + from[j].length)-1;
+                
+                break;
+            }
+        }
+        
+        if (false !== match) {
+            ret += to[j];
+        } else {
+            ret += str[i];
+        }
+    }
+
+    return ret;
+}
+
