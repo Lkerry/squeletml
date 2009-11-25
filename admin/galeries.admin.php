b9c2b2f9a60bcc707681eae70da25ec586097111
Jean-Philippe Fleury <contact@jpfleury.net>
2009-11-25 11:54:37 -0500

    On vérifie lors de la création d'une page web de galerie que l'emplacement est gérable.

--- admin/galeries.admin.php
+++ admin/galeries.admin.php
@@ -754,6 +754,10 @@ include '../init.inc.php';
 		{
 			$messagesScript[] = '<li class="erreur">' . sprintf(T_("La galerie %1\$s n'existe pas."), "<code>$id</code>") . "</li>\n";
 		}
+		elseif (!adminEmplacementPermis($cheminPage, $adminTypeFiltreDossiers, $tableauFiltresDossiers))
+		{
+			$messagesScript[] = '<li class="erreur">' . sprintf(T_("L'emplacement spécifié pour la création d'une page web de galerie (%1\$s) n'est pas gérable par le porte-documents."), "<code>$cheminPage</code>") . "</li>\n";
+		}
 		else
 		{
 			$cheminConfigGalerie = adminCheminConfigGalerie($racine, $id);
