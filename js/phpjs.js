3389312487afd92686d44b4bf1ed9e9b67576a38
Jean-Philippe Fleury <contact@jpfleury.net>
2009-12-11 13:23:10 -0500

    Mise à jour de `krsort()` et de `strtr()` de PHPJS pour régler le bogue de génération de la table des matières sous IE.

--- js/phpjs.js
+++ js/phpjs.js
@@ -1,79 +1,85 @@
 /* 
  * More info at: http://phpjs.org
  * 
- * This is version: 2.94
+ * This is version: 3.01
  * php.js is copyright 2009 Kevin van Zonneveld.
  * 
  * Portions copyright Brett Zamir (http://brett-zamir.me), Kevin van Zonneveld
  * (http://kevin.vanzonneveld.net), Onno Marsman, Michael White
- * (http://getsprink.com), Waldo Malqui Silva, Paulo Ricardo F. Santos, Jack,
- * Jonas Raoni Soares Silva (http://www.jsfromhell.com), Philip Peterson, Ates
- * Goral (http://magnetiq.com), Legaev Andrey, Ratheous, Alex, Martijn
- * Wieringa, Nate, lmeyrick (https://sourceforge.net/projects/bcmath-js/),
- * Philippe Baumann, Enrique Gonzalez, Webtoolkit.info
- * (http://www.webtoolkit.info/), Theriault, Ash Searle
- * (http://hexmen.com/blog/), Jani Hartikainen, travc, Ole Vrijenhoek, Carlos
- * R. L. Rodrigues (http://www.jsfromhell.com), stag019, pilus,
+ * (http://getsprink.com), Waldo Malqui Silva, Paulo Ricardo F. Santos,
+ * Theriault, Jack, Jonas Raoni Soares Silva (http://www.jsfromhell.com),
+ * Philip Peterson, Ates Goral (http://magnetiq.com), Legaev Andrey, Ratheous,
+ * Alex, Martijn Wieringa, Nate, lmeyrick
+ * (https://sourceforge.net/projects/bcmath-js/), Enrique Gonzalez, Philippe
+ * Baumann, Webtoolkit.info (http://www.webtoolkit.info/), Ole Vrijenhoek, Ash
+ * Searle (http://hexmen.com/blog/), Jani Hartikainen, Carlos R. L. Rodrigues
+ * (http://www.jsfromhell.com), travc, WebDevHobo
+ * (http://webdevhobo.blogspot.com/), stag019, GeekFG
+ * (http://geekfg.blogspot.com),
  * http://stackoverflow.com/questions/57803/how-to-convert-decimal-to-hex-in-javascript,
- * Michael Grier, marrtins, d3x, Andrea Giammarchi
- * (http://webreflection.blogspot.com), GeekFG (http://geekfg.blogspot.com),
- * Erkekjetter, Johnny Mast (http://www.phpvrouwen.nl), T.Wild, majak, David,
- * Oleg Eremeev, mdsjack (http://www.mdsjack.bo.it), Breaking Par Consulting
- * Inc
- * (http://www.breakingpar.com/bkp/home.nsf/0/87256B280015193F87256CFB006C45F7),
- * Mirek Slugen, Martin (http://www.erlenwiese.de/), Public Domain
- * (http://www.json.org/json2.js), Joris, Steven Levithan
- * (http://blog.stevenlevithan.com), Steve Hilder, KELAN, Arpad Ray
- * (mailto:arpad@php.net), T.J. Leahy, Marc Palau, Josh Fraser
+ * T.Wild, Erkekjetter, pilus, d3x, Johnny Mast (http://www.phpvrouwen.nl),
+ * Michael Grier, marrtins, Andrea Giammarchi
+ * (http://webreflection.blogspot.com), Felix Geisendoerfer
+ * (http://www.debuggable.com/felix), Martin (http://www.erlenwiese.de/), Marc
+ * Palau, Michael White, Kankrelune (http://www.webfaktory.info/), Public
+ * Domain (http://www.json.org/json2.js), majak, gettimeofday, Steven Levithan
+ * (http://blog.stevenlevithan.com), Pellentesque Malesuada, Josh Fraser
  * (http://onlineaspect.com/2007/06/08/auto-detect-a-time-zone-with-javascript/),
- * gettimeofday, AJ, Aman Gupta, Felix Geisendoerfer
- * (http://www.debuggable.com/felix), Sakimori, Lars Fischer, Caio Ariede
- * (http://caioariede.com), Alfonso Jimenez (http://www.alfonsojimenez.com),
- * Pellentesque Malesuada, Tyler Akins (http://rumkin.com), gorthaur,
- * Thunder.m, Karol Kowalski, Kankrelune (http://www.webfaktory.info/), Frank
- * Forte, Subhasis Deb, duncan, Gilbert, class_exists, noname, Marco, madipta,
- * 0m3r, David James, Arno, Nathan, Mateusz "loonquawl" Zalega, ReverseSyntax,
- * Scott Cariss, Slawomir Kaniecki, Denny Wardhana, nobbler, sankai, Sanjoy
- * Roy, Douglas Crockford (http://javascript.crockford.com), mktime, marc
- * andreu, ger, john (http://www.jd-tech.net), Ole Vrijenhoek
- * (http://www.nervous.nl/), Steve Clay, Thiago Mata
- * (http://thiagomata.blog.com), Jon Hohle, Linuxworld, lmeyrick
- * (https://sourceforge.net/projects/bcmath-js/this.), Ozh, nord_ua, Pyerre,
- * Soren Hansen, Peter-Paul Koch (http://www.quirksmode.org/js/beat.html),
- * T0bsn, MeEtc (http://yass.meetcweb.com), Brad Touesnard, David Randall,
- * Bryan Elliott, Tim Wiel, XoraX (http://www.xorax.info), djmix, Paul, J A R,
- * Hyam Singer (http://www.impact-computing.com/), kenneth, T. Wild, Raphael
- * (Ao RUDLER), Marc Jansen, Francesco, Lincoln Ramsay, echo is bad, Der Simon
- * (http://innerdom.sourceforge.net/), Eugene Bulkin (http://doubleaw.com/),
- * LH, JB, Bayron Guevara, Cord, Francois, Kristof Coomans (SCK-CEN Belgian
- * Nucleair Research Centre), Pierre-Luc Paour, Martin Pool, Kirk Strobeck,
- * Saulo Vallory, Christoph, Artur Tchernychev, Wagner B. Soares, Valentina De
- * Rosa, Daniel Esteban, Jason Wong (http://carrot.org/), Rick Waldron,
- * Mick@el, Anton Ongson, Simon Willison (http://simonwillison.net), Gabriel
- * Paderni, Marco van Oort, Blues (http://tech.bluesmoon.info/), Luke Godfrey,
- * rezna, Tomasz Wesolowski, Eric Nagel, Pul, Bobby Drake, uestla, Alan C,
- * Yves Sucaet, sowberry, hitwork, Norman "zEh" Fuchs, Ulrich, johnrembo, Nick
- * Callen, ejsanders, Aidan Lister (http://aidanlister.com/), Brian Tafoya
+ * Lars Fischer, Joris, Arpad Ray (mailto:arpad@php.net), Breaking Par
+ * Consulting Inc
+ * (http://www.breakingpar.com/bkp/home.nsf/0/87256B280015193F87256CFB006C45F7),
+ * KELAN, Mirek Slugen, AJ, Alfonso Jimenez (http://www.alfonsojimenez.com),
+ * Caio Ariede (http://caioariede.com), Mailfaker (http://www.weedem.fr/),
+ * Tyler Akins (http://rumkin.com), Aman Gupta, Thunder.m, mdsjack
+ * (http://www.mdsjack.bo.it), Oleg Eremeev, Sakimori, Karol Kowalski,
+ * gorthaur, Steve Hilder, David, Francois, David James, Steve Clay,
+ * class_exists, Marco, noname, madipta, sankai, Slawomir Kaniecki, Frank
+ * Forte, Nathan, T. Wild, ger, nobbler, marc andreu, john
+ * (http://www.jd-tech.net), Arno, ReverseSyntax, Scott Cariss, Mateusz
+ * "loonquawl" Zalega, Douglas Crockford (http://javascript.crockford.com),
+ * Denny Wardhana, mktime, Marc Jansen, Ole Vrijenhoek
+ * (http://www.nervous.nl/), T0bsn, Gilbert, Peter-Paul Koch
+ * (http://www.quirksmode.org/js/beat.html), MeEtc (http://yass.meetcweb.com),
+ * Bryan Elliott, Tim Wiel, Brad Touesnard, Soren Hansen, duncan, djmix,
+ * Lincoln Ramsay, Bayron Guevara, lmeyrick
+ * (https://sourceforge.net/projects/bcmath-js/this.), Linuxworld, Pyerre, Jon
+ * Hohle, Thiago Mata (http://thiagomata.blog.com), David Randall, Subhasis
+ * Deb, J A R, 0m3r, Francesco, Paul, Hyam Singer
+ * (http://www.impact-computing.com/), Raphael (Ao RUDLER), Sanjoy Roy,
+ * kenneth, Stoyan Kyosev (http://www.svest.org/), LH, Ozh, nord_ua, date,
+ * XoraX (http://www.xorax.info), echo is bad, JB, Eugene Bulkin
+ * (http://doubleaw.com/), Der Simon (http://innerdom.sourceforge.net/),
+ * Manish, Itsacon (http://www.itsacon.net/), Pierre-Luc Paour, Martin Pool,
+ * Kirk Strobeck, Rick Waldron, Kristof Coomans (SCK-CEN Belgian Nucleair
+ * Research Centre), Saulo Vallory, Wagner B. Soares, Valentina De Rosa, Jason
+ * Wong (http://carrot.org/), Christoph, Daniel Esteban, Mick@el, rezna, Simon
+ * Willison (http://simonwillison.net), Gabriel Paderni, Marco van Oort,
+ * penutbutterjelly, Philipp Lenssen, Anton Ongson, Blues
+ * (http://tech.bluesmoon.info/), Tomasz Wesolowski, Eric Nagel, Bobby Drake,
+ * Luke Godfrey, Pul, Artur Tchernychev, uestla, Yves Sucaet, sowberry,
+ * hitwork, Orlando, Norman "zEh" Fuchs, Ulrich, johnrembo, Nick Callen,
+ * ejsanders, Aidan Lister (http://aidanlister.com/), Brian Tafoya
  * (http://www.premasolutions.com/), Philippe Jausions
- * (http://pear.php.net/user/jausions), Orlando, dptr1988, HKM, metjay,
- * strcasecmp, strcmp, Taras Bogach, ChaosNo1, Alexander Ermolaev
+ * (http://pear.php.net/user/jausions), kilops, dptr1988, HKM, metjay,
+ * strcasecmp, strcmp, Alan C, Taras Bogach, ChaosNo1, Alexander Ermolaev
  * (http://snippets.dzone.com/user/AlexanderErmolaev), Le Torbi, James, Chris,
- * DxGx, Pedro Tainha (http://www.pedrotainha.com), Philipp Lenssen,
- * penutbutterjelly, Greg Frazier, Tod Gentille, Alexander M Beedie,
- * FremyCompany, baris ozdil, FGFEmperor, Atli Þór, 3D-GRAF, jakes, gabriel
- * paderni, Yannoo, Luis Salazar (http://www.freaky-media.com/), Tim de
- * Koning, stensi, vlado houba, Jalal Berrami, date, Matteo, Victor, taith,
- * Robin, Matt Bradley, fearphage (http://http/my.opera.com/fearphage/),
- * Manish, davook, Benjamin Lupton, Russell Walker (http://www.nbill.co.uk/),
- * Garagoth, Andrej Pavlovic, Dino, Jamie Beck (http://www.terabit.ca/), DtTvB
- * (http://dt.in.th/2008-09-16.string-length-in-bytes.html), Christian
- * Doebler, setcookie, YUI Library:
- * http://developer.yahoo.com/yui/docs/YAHOO.util.DateLocale.html, Andreas,
- * Blues at http://hacks.bluesmoon.info/strftime/strftime.js, Greenseed,
- * mk.keck, Luke Smith (http://lucassmith.name), Rival, Diogo Resende, Allan
- * Jensen (http://www.winternet.no), Howard Yeend, Kheang Hok Chin
- * (http://www.distantia.ca/), Jay Klehr, Leslie Hoare, Ben Bryan, booeyOH,
- * Amir Habibi (http://www.residence-mixte.com/), Cagri Ekin
+ * DxGx, Pedro Tainha (http://www.pedrotainha.com), Christian Doebler,
+ * setcookie, Greg Frazier, Tod Gentille, Alexander M Beedie, T.J. Leahy,
+ * baris ozdil, FGFEmperor, daniel airton wermann (http://wermann.com.br),
+ * 3D-GRAF, jakes, gabriel paderni, Yannoo, FremyCompany, Luis Salazar
+ * (http://www.freaky-media.com/), Matteo, stensi, Billy, Jalal Berrami, vlado
+ * houba, Victor, fearphage (http://http/my.opera.com/fearphage/), Tim de
+ * Koning, taith, Robin, Cord, Matt Bradley, Atli Þór, Maximusya, Andrej
+ * Pavlovic, Dino, rem, mk.keck, Greenseed, Garagoth, Russell Walker
+ * (http://www.nbill.co.uk/), YUI Library:
+ * http://developer.yahoo.com/yui/docs/YAHOO.util.DateLocale.html, Blues at
+ * http://hacks.bluesmoon.info/strftime/strftime.js, Andreas, Jamie Beck
+ * (http://www.terabit.ca/), DtTvB
+ * (http://dt.in.th/2008-09-16.string-length-in-bytes.html), Leslie Hoare, Ben
+ * Bryan, Diogo Resende, Howard Yeend, Allan Jensen (http://www.winternet.no),
+ * davook, Benjamin Lupton, Rival, Luke Smith (http://lucassmith.name),
+ * booeyOH, Cagri Ekin, Amir Habibi (http://www.residence-mixte.com/), Kheang
+ * Hok Chin (http://www.distantia.ca/), Jay Klehr
  * 
  * Dual licensed under the MIT (MIT-LICENSE.txt)
  * and GPL (GPL-LICENSE.txt) licenses.
@@ -98,20 +104,6 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */ 
 
-function ucfirst (str) {
-    // Makes a string's first character uppercase  
-    // 
-    // version: 909.322
-    // discuss at: http://phpjs.org/functions/ucfirst
-    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Onno Marsman
-    // +   improved by: Brett Zamir (http://brett-zamir.me)
-    // *     example 1: ucfirst('kevin van zonneveld');
-    // *     returns 1: 'Kevin van zonneveld'
-    str += '';
-    var f = str.charAt(0).toUpperCase();
-    return f + str.substr(1);
-}
 
 function get_html_translation_table (table, quote_style) {
     // Returns the internal translation table used by htmlspecialchars and htmlentities  
@@ -358,21 +350,69 @@ function i18n_loc_set_default (name) {
     return true;
 }
 
-function krsort (array, sort_flags) {
-    // Sort an array by key value in reverse order  
+function ini_set (varname, newvalue) {
+    // Set a configuration option, returns false on error and the old value of the configuration option on success  
     // 
-    // version: 909.322
-    // discuss at: http://phpjs.org/functions/krsort
+    // version: 911.2923
+    // discuss at: http://phpjs.org/functions/ini_set
+    // +   original by: Brett Zamir (http://brett-zamir.me)
+    // %        note 1: This will not set a global_value or access level for the ini item
+    // *     example 1: ini_set('date.timezone', 'America/Chicago');
+    // *     returns 1: 'Asia/Hong_Kong'
+
+    var oldval = '', that = this;
+    this.php_js = this.php_js || {};
+    this.php_js.ini = this.php_js.ini || {};
+    this.php_js.ini[varname] = this.php_js.ini[varname] || {};
+    oldval = this.php_js.ini[varname].local_value;
+    
+    var _setArr = function (oldval) { // Although these are set individually, they are all accumulated
+        if (typeof oldval === 'undefined') {
+            that.php_js.ini[varname].local_value = [];
+        }
+        that.php_js.ini[varname].local_value.push(newvalue);
+    };
+
+    switch (varname) {
+        case 'extension':
+            if (typeof this.dl === 'function') {
+                this.dl(newvalue); // This function is only experimental in php.js
+            }
+            _setArr(oldval, newvalue);
+            break;
+        default:
+            this.php_js.ini[varname].local_value = newvalue;
+            break;
+    }
+    return oldval;
+}
+
+function krsort (inputArr, sort_flags) {
+    // http://kevin.vanzonneveld.net
     // +   original by: GeekFG (http://geekfg.blogspot.com)
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +   improved by: Brett Zamir (http://brett-zamir.me)
-    // %          note: The examples are correct, this is a new way
+    // %          note 1: The examples are correct, this is a new way
+    // %        note 2: This function deviates from PHP in returning a copy of the array instead
+    // %        note 2: of acting by reference and returning true; this was necessary because
+    // %        note 2: IE does not allow deleting and re-adding of properties without caching
+    // %        note 2: of property position; you can set the ini of "phpjs.strictForIn" to true to
+    // %        note 2: get the PHP behavior, but use this only if you are in an environment
+    // %        note 2: such as Firefox extensions where for-in iteration order is fixed and true
+    // %        note 2: property deletion is supported. Note that we intend to implement the PHP
+    // %        note 2: behavior by default if IE ever does allow it; only gives shallow copy since
+    // %        note 2: is by reference in PHP anyways
     // -    depends on: i18n_loc_get_default
-    // *     example 1: data = {2: 'van', 3: 'Zonneveld', 1: 'Kevin'};
-    // *     example 1: krsort(data);
-    // *     results 1: data == {3: 'Kevin', 2: 'van', 1: 'Zonneveld'}
-    // *     returns 1: true
-    var tmp_arr={}, keys=[], sorter, i, key, that=this;
+    // *     example 1: data = {d: 'lemon', a: 'orange', b: 'banana', c: 'apple'};
+    // *     example 1: data = krsort(data);
+    // *     results 1: {d: 'lemon', c: 'apple', b: 'banana', a: 'orange'}
+    // *     example 2: ini_set('phpjs.strictForIn', true);
+    // *     example 2: data = {2: 'van', 3: 'Zonneveld', 1: 'Kevin'};
+    // *     example 2: krsort(data);
+    // *     results 2: data == {3: 'Kevin', 2: 'van', 1: 'Zonneveld'}
+    // *     returns 2: true
+
+    var tmp_arr={}, keys=[], sorter, i, k, that=this, strictForIn = false, populateArr = [];
 
     switch (sort_flags) {
         case 'SORT_STRING': // compare items as strings
@@ -404,30 +444,41 @@ function krsort (array, sort_flags) {
     }
 
     // Make a list of key names
-    for (key in array) {
-        keys.push(key);
+    for (k in inputArr) {
+        if (inputArr.hasOwnProperty) {
+            keys.push(k);
+        }
     }
-
     keys.sort(sorter);
+
+    // BEGIN REDUNDANT
+    this.php_js = this.php_js || {};
+    this.php_js.ini = this.php_js.ini || {};
+    // END REDUNDANT
+
+    strictForIn = this.php_js.ini['phpjs.strictForIn'] && this.php_js.ini['phpjs.strictForIn'].local_value;
+    populateArr = strictForIn ? inputArr : populateArr;
+
 
     // Rebuild array with sorted key names
     for (i = 0; i < keys.length; i++) {
-        key = keys[i];
-        tmp_arr[key] = array[key];
-        delete array[key];
+        k = keys[i];
+        tmp_arr[k] = inputArr[k];
+        if (strictForIn) {
+            delete inputArr[k];
+        }
     }
     for (i in tmp_arr) {
-        array[i] = tmp_arr[i];
+        if (tmp_arr.hasOwnProperty) {
+            populateArr[i] = tmp_arr[i];
+        }
     }
 
-    return true;
+    return strictForIn ? true : populateArr;
 }
 
 function strtr (str, from, to) {
-    // Translates characters in str using given translation tables  
-    // 
-    // version: 909.322
-    // discuss at: http://phpjs.org/functions/strtr
+    // http://kevin.vanzonneveld.net
     // +   original by: Brett Zamir (http://brett-zamir.me)
     // +      input by: uestla
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
@@ -435,7 +486,10 @@ function strtr (str, from, to) {
     // +   bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: Taras Bogach
     // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
+    // +      input by: jpfle
+    // +   bugfixed by: Brett Zamir (http://brett-zamir.me)
     // -   depends on: krsort
+    // -   depends on: ini_set
     // *     example 1: $trans = {'hello' : 'hi', 'hi' : 'hello'};
     // *     example 1: strtr('hi all, I said hello', $trans)
     // *     returns 1: 'hello all, I said hi'
@@ -449,48 +503,79 @@ function strtr (str, from, to) {
     // *     returns 5: 'http'
     // *     example 6: strtr('aa', {'a':1,'aa':2});
     // *     returns 6: '2'
-    var fr = '', i = 0, j = 0, lenStr = 0, lenFrom = 0;
+
+    var fr = '', i = 0, j = 0, lenStr = 0, lenFrom = 0, tmpStrictForIn = false, fromTypeStr = '', toTypeStr = '', istr = '';
     var tmpFrom = [];
-    var tmpTo   = [];
+    var tmpTo = [];
     var ret = '';
     var match = false;
 
     // Received replace_pairs?
     // Convert to normal from->to chars
     if (typeof from === 'object') {
-        this.krsort(from);
+        tmpStrictForIn = this.ini_set('phpjs.strictForIn', false); // Not thread-safe; temporarily set to true
+        from = this.krsort(from);
+        this.ini_set('phpjs.strictForIn', tmpStrictForIn);
+
         for (fr in from) {
-            tmpFrom.push(fr);
-            tmpTo.push(from[fr]);
+            if (from.hasOwnProperty(fr)) {
+                tmpFrom.push(fr);
+                tmpTo.push(from[fr]);
+            }
         }
 
         from = tmpFrom;
-        to   = tmpTo;
+        to = tmpTo;
     }
     
     // Walk through subject and replace chars when needed
     lenStr  = str.length;
     lenFrom = from.length;
+    fromTypeStr = typeof from === 'string';
+    toTypeStr = typeof to === 'string';
+
     for (i = 0; i < lenStr; i++) {
         match = false;
-        for (j = 0; j < lenFrom; j++) {
-            if (str.substr(i, from[j].length) == from[j]) {
-                match = true;
-
-                // Fast forward
-                i = (i + from[j].length)-1;
-                
-                break;
+        if (fromTypeStr) {
+            istr = str.charAt(i);
+            for (j = 0; j < lenFrom; j++) {
+                if (istr == from.charAt(j)) {
+                    match = true;
+                    break;
+                }
             }
         }
-        
-        if (false !== match) {
-            ret += to[j];
+        else {
+            for (j = 0; j < lenFrom; j++) {
+                if (str.substr(i, from[j].length) == from[j]) {
+                    match = true;
+                    // Fast forward
+                    i = (i + from[j].length)-1;
+                    break;
+                }
+            }
+        }
+        if (match) {
+            ret += toTypeStr ? to.charAt(j) : to[j];
         } else {
-            ret += str[i];
+            ret += str.charAt(i);
         }
     }
 
     return ret;
 }
 
+function ucfirst (str) {
+    // Makes a string's first character uppercase  
+    // 
+    // version: 909.322
+    // discuss at: http://phpjs.org/functions/ucfirst
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // *     example 1: ucfirst('kevin van zonneveld');
+    // *     returns 1: 'Kevin van zonneveld'
+    str += '';
+    var f = str.charAt(0).toUpperCase();
+    return f + str.substr(1);
+}
