c3f6dcc9a5c70111b6c4ca1b6321151d86e8558a
Jean-Philippe Fleury <contact@jpfleury.net>
2009-12-16 11:49:07 -0500

    CodeMirror par défaut

--- admin/inc/config.inc.php
+++ admin/inc/config.inc.php
@@ -216,5 +216,5 @@ $adminPorteDocumentsDroits = array (
 - Pour désactiver l'aide, laisser vide, c'est-à-dire:
   $adminAideEdition = '';
 */
-$adminAideEdition = 'BUEditor'; // BUEditor|CodeMirror
+$adminAideEdition = 'CodeMirror'; // BUEditor|CodeMirror
 ?>
