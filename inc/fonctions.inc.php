47a60d22a9e969a5e8afb92465e92976e187fcec
Jean-Philippe Fleury <contact@jpfleury.net>
2011-01-07 10:57:13 -0500

    Ajout de `.text` dans les extensions protégées.

--- inc/fonctions.inc.php
+++ inc/fonctions.inc.php
@@ -59,7 +59,7 @@ function accesDansHtaccess($racine, $serveurFreeFr)
 			$htaccess .= "# Ajout automatique de Squeletml (accès admin). Ne pas modifier.\n";
 			$htaccess .= "# Empêcher l'affichage direct de certains fichiers.\n";
 		
-			$htaccessFilesModele = "(ChangeLog|\.acces|\.admin\.php|\.cache\.gif|\.cache\.html|\.cache\.jpeg|\.cache\.jpg|\.cache\.png|\.cache\.xml|\.ini|\.mdtxt|\.modele|\.sauv|\.txt)$";
+			$htaccessFilesModele = "(ChangeLog|\.acces|\.admin\.php|\.cache\.gif|\.cache\.html|\.cache\.jpeg|\.cache\.jpg|\.cache\.png|\.cache\.xml|\.ini|\.mdtxt|\.modele|\.sauv|\.text|\.txt)$";
 		
 			if ($serveurFreeFr)
 			{
