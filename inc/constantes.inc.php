4a40551dfb61cff43254b4fdca5273d6255943bf
Jean-Philippe Fleury <contact@jpfleury.net>
2011-01-07 09:11:49 -0500

    - Ajout de la possibilité d'imbriquer du code PHP dans une chaîne écrite en Markdown (et pas seulement dans un fichier) et possibilité d'écrire le code PHP sur plusieurs lignes. - Maj de la doc.

--- inc/constantes.inc.php
+++ inc/constantes.inc.php
@@ -9,34 +9,25 @@ define('LANGUE', langue($langue, $langueParDefaut));
 
 // Le code de la constante `MDTXT_PHP` est utilisé pour évaluer du code PHP imbriqué dans du Markdown (voir la section «Syntaxe Markdown avec imbrication de code PHP» dans la documentation). La raison pour laquelle une fonction n'a pas été utilisée est de pouvoir profiter de la même portée pour les variables que celle dans le corps d'une page de Squeletml sans devoir passer par des variables globales.
 define('MDTXT_PHP', <<<CODE
-	if (isset(\$cheminMdtxtPhp) && \$ficMdtxtPhp = @fopen(\$cheminMdtxtPhp, 'r'))
+	if (!isset(\$chaineMdtxtPhp) && isset(\$cheminMdtxtPhp))
 	{
-		\$chaineMdtxtPhp = '';
+		\$chaineMdtxtPhp = @file_get_contents(\$cheminMdtxtPhp);
+	}
+	
+	if (isset(\$chaineMdtxtPhp) && \$chaineMdtxtPhp !== FALSE && preg_match_all('/\[php\](.+?)\[\/php\]/s', \$chaineMdtxtPhp, \$resultatMdtxtPhp))
+	{
+		\$iMdtxtPhp = 0;
 		
-		while (!feof(\$ficMdtxtPhp))
+		foreach (\$resultatMdtxtPhp[1] as \$phpAevaluerMdtxtPhp)
 		{
-			\$ligneMdtxtPhp = fgets(\$ficMdtxtPhp);
+			ob_start();
+			eval(\$phpAevaluerMdtxtPhp);
+			\$phpEvalueMdtxtPhp = ob_get_contents();
+			ob_end_clean();
 			
-			if (preg_match_all('/\[php\](.+?)\[\/php\]/', \$ligneMdtxtPhp, \$resultatMdtxtPhp))
-			{
-				\$iMdtxtPhp = 0;
-				
-				foreach (\$resultatMdtxtPhp[1] as \$phpAevaluerMdtxtPhp)
-				{
-					ob_start();
-					eval(\$phpAevaluerMdtxtPhp);
-					\$phpEvalueMdtxtPhp = ob_get_contents();
-					ob_end_clean();
-					
-					\$ligneMdtxtPhp = str_replace(\$resultatMdtxtPhp[0][\$iMdtxtPhp], \$phpEvalueMdtxtPhp, \$ligneMdtxtPhp);
-					\$iMdtxtPhp++;
-				}
-			}
-			
-			\$chaineMdtxtPhp .= \$ligneMdtxtPhp;
+			\$chaineMdtxtPhp = str_replace(\$resultatMdtxtPhp[0][\$iMdtxtPhp], \$phpEvalueMdtxtPhp, \$chaineMdtxtPhp);
+			\$iMdtxtPhp++;
 		}
-		
-		fclose(\$ficMdtxtPhp);
 		
 		echo mdtxtChaine(\$chaineMdtxtPhp);
 	}
