1a8d466fd74c8d9da47c573feae5beeefb03f196
Jean-Philippe Fleury <contact@jpfleury.net>
2011-06-22 13:06:59 -0400

    Constante `MDTXT_PHP` modifiée pour `MKD_PHP`.

--- inc/constantes.inc.php
+++ inc/constantes.inc.php
@@ -7,8 +7,8 @@ if (!defined('LC_MESSAGES'))
 define('ACCUEIL', accueil($accueil, array ($langue, $langueParDefaut)));
 define('LANGUE', langue($langue, $langueParDefaut));
 
-// Le code de la constante `MDTXT_PHP` est utilisé pour évaluer du code PHP imbriqué dans du Markdown (voir la section «Syntaxe Markdown avec imbrication de code PHP» dans la documentation). La raison pour laquelle une fonction n'a pas été utilisée est de pouvoir profiter de la même portée pour les variables que celle dans le corps d'une page de Squeletml sans devoir passer par des variables globales.
-define('MDTXT_PHP', <<<CODE
+// Le code de la constante `MKD_PHP` est utilisé pour évaluer du code PHP imbriqué dans du Markdown (voir la section «Syntaxe Markdown avec imbrication de code PHP» dans la documentation). La raison pour laquelle une fonction n'a pas été utilisée est de pouvoir profiter de la même portée pour les variables que celle dans le corps d'une page de Squeletml sans devoir passer par des variables globales.
+define('MKD_PHP', <<<CODE
 	if (!isset(\$chaineMkdPhp) && isset(\$cheminMkdPhp))
 	{
 		\$chaineMkdPhp = @file_get_contents(\$cheminMkdPhp);
