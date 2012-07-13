1bb067788e327bd0fb0305a56645941e210082e2
Jean-Philippe Fleury <contact@jpfleury.net>
2012-07-12 22:45:40 -0400

    Langue de la page globale des galeries selon le navigateur

--- inc/premier.inc.php
+++ inc/premier.inc.php
@@ -46,7 +46,12 @@ eval(variablesAaffecterAuDebut());
 $estPageCompte = $urlSansGet == "$urlRacine/compte.php" ? TRUE : FALSE;
 $estPageDeconnexion = $urlSansGet == "$urlRacine/deconnexion.php" ? TRUE : FALSE;
 
-if ($estPageCompte || $estPageDeconnexion)
+if (!isset($pageGlobaleGalerie))
+{
+	$pageGlobaleGalerie = FALSE;
+}
+
+if ($estPageCompte || $estPageDeconnexion || $pageGlobaleGalerie)
 {
 	$langue = langue('navigateur', '');
 }
@@ -118,7 +123,7 @@ if ($dureeCache && !$desactiverCache)
 // Affectations 2 de 3.
 
 extract(init('', 'baliseH1', 'boitesDeroulantes', 'classesBody', 'classesContenu', 'courrielContact', 'dateCreation', 'dateRevision', 'description', 'enTetesHttp', 'idGalerie', 'idGalerieDossier', 'motsCles', 'robots'), EXTR_SKIP);
-extract(init(FALSE, 'envoyerAmisEstActif', 'envoyerAmisInclureContact', 'erreur404', 'estPageDerreur', 'pageGlobaleGalerie', 'titreGalerieGenere'), EXTR_SKIP);
+extract(init(FALSE, 'envoyerAmisEstActif', 'envoyerAmisInclureContact', 'erreur404', 'estPageDerreur', 'titreGalerieGenere'), EXTR_SKIP);
 
 if (!isset($apercu))
 {
