85eb2eead77aef7bdfde4ebd1a798b667efe0c6d
Jean-Philippe Fleury <contact@jpfleury.net>
2012-07-16 22:11:39 -0400

    Continuation de la traduction en anglais

--- inc/fonctions.inc.php
+++ inc/fonctions.inc.php
@@ -3067,31 +3067,27 @@ function licence($urlRacine, $choixLicence)
 			break;
 
 		case 'cc-by':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Attribution 3.0 non transposé (CC BY 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'cc-by-sa':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-sa/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité – Partage des conditions initiales à l'identique 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by-sa/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-sa/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-sa/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Paternité – Partage dans les mêmes conditions 3.0 non transposé (CC BY-SA 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by-sa/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-sa/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'cc-by-nd':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nd/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité – Pas de modification 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by-nd/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nd/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nd/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Attribution – Pas de modification 3.0 non transposé (CC BY-ND 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by-nd/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nd/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'cc-by-nc':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité – Pas d'utilisation commerciale 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by-nc/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Attribution – Pas d'utilisation commerciale 3.0 non transposé (CC BY-NC 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by-nc/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'cc-by-nc-sa':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité – Pas d'utilisation commerciale – Partage des conditions initiales à l'identique 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Attribution – Pas d'utilisation commerciale – Partage dans les mêmes conditions 3.0 non transposé (CC BY-NC-SA 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by-nc-sa/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'cc-by-nc-nd':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr\"><img %1\$s alt=\"Contrat Creative Commons Paternité – Pas d'utilisation commerciale – Pas de modification 3.0 Générique\" /></a> Mis à disposition sous un <a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr\">contrat Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
-			break;
-
-		case 'dp':
-			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/publicdomain/deed.fr\"><img %1\$s alt=\"Domaine public\" /></a> Mis à disposition dans le <a href=\"http://creativecommons.org/licenses/publicdomain/deed.fr\">domaine public</a>."), "src=\"$urlRacine/fichiers/domaine-public-80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr\"><img %1\$s alt=\"Licence Creative Commons Attribution – Pas d'utilisation commerciale – Pas de modification 3.0 non transposé (CC BY-NC-ND 3.0)\" /></a> Mis à disposition sous une <a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr\">licence Creative Commons</a>."), "src=\"http://i.creativecommons.org/l/by-nc-nd/3.0/80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'gplv2':
@@ -3138,8 +3134,16 @@ function licence($urlRacine, $choixLicence)
 			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://fr.wikipedia.org/wiki/Licence_BSD#Texte_de_la_licence\"><img %1\$s alt=\"Licence BSD modifiée\" /></a> Mis à disposition sous la <a href=\"http://fr.wikipedia.org/wiki/Licence_BSD#Texte_de_la_licence\">licence BSD modifiée</a>."), "src=\"$urlRacine/fichiers/licence-bsd-modifiee-80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
+		case 'mdp':
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/publicdomain/mark/1.0/deed.fr\"><img %1\$s alt=\"Marque du domaine public 1.0\" /></a> Identifié comme étant dans le <a href=\"http://creativecommons.org/publicdomain/mark/1.0/deed.fr\">domaine public</a>."), "src=\"$urlRacine/fichiers/domaine-public-80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			break;
+
 		case 'mit':
 			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://www.opensource.org/licenses/mit-license.php\"><img %1\$s alt=\"Licence MIT\" /></a> Mis à disposition sous la <a href=\"http://www.opensource.org/licenses/mit-license.php\">licence MIT</a>."), "src=\"$urlRacine/fichiers/licence-mit-80x15.png\" width=\"80\" height=\"15\"") . '</span>';
+			break;
+
+		case 'tdp':
+			$licence = '<span class="licence">' . sprintf(T_("<a href=\"http://creativecommons.org/publicdomain/zero/1.0/deed.fr\"><img %1\$s alt=\"Transfert dans le domaine public – CC0 1.0 universel (CC0 1.0)\" /></a> Transféré dans le <a href=\"http://creativecommons.org/publicdomain/zero/1.0/deed.fr\">domaine public</a>."), "src=\"$urlRacine/fichiers/domaine-public-80x15.png\" width=\"80\" height=\"15\"") . '</span>';
 			break;
 
 		case 'verbatim':
