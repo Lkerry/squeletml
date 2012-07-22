91518cc624273914acaafedd53b546409cae1f4c
Jean-Philippe Fleury <contact@jpfleury.net>
2012-07-21 20:00:26 -0400

    Maj de la doc + maj des fichiers protégés

--- inc/fonctions.inc.php
+++ inc/fonctions.inc.php
@@ -59,7 +59,7 @@ function accesDansHtaccess($racine, $serveurFreeFr)
 			$htaccess .= "# Ajout automatique de Squeletml (accès admin). Ne pas modifier.\n";
 			$htaccess .= "# Empêcher l'affichage direct de certains fichiers.\n";
 		
-			$htaccessFilesModele = "(\.acces|\.admin\.php|\.cache\.gif|\.cache\.html|\.cache\.jpeg|\.cache\.jpg|\.cache\.png|\.cache\.xml|\.ini|\.mkd|\.modele|\.sauv|\.text|\.txt)$";
+			$htaccessFilesModele = "(Makefile|\.acces|\.admin\.php|\.cache\.gif|\.cache\.html|\.cache\.jpeg|\.cache\.jpg|\.cache\.png|\.cache\.xml|\.gitattributes|\.gitignore|\.ini|\.mkd|\.mo|\.modele|\.po|\.po\.info|\.pot|\.sauv|scripts\.cli\.php|\.text|\.txt)$";
 		
 			if ($serveurFreeFr)
 			{
