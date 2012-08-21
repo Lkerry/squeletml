b7f25f60623bc5d30908289b208188b183924c7a
Jean-Philippe Fleury <contact@jpfleury.net>
2012-08-21 17:11:26 -0400

    Note sur la modification dans le formulaire des commentaires

--- admin/commentaires.admin.php
+++ admin/commentaires.admin.php
@@ -595,7 +595,7 @@ include $racineAdmin . '/inc/premier.inc.php';
 				
 				$contenuFormulaire .= '<p>' . T_("Pour supprimer un commentaire, simplement effacer tout le contenu du message associé.") . "</p>\n";
 				
-				$contenuFormulaire .= '<p>' . T_("Prendre note que modifier le courriel dans ce formulaire ou supprimer un commentaire ne va pas modifier l'abonnement aux notifications, s'il y a lieu.") . "</p>\n";
+				$contenuFormulaire .= '<p>' . T_("Prendre note que les modifications dans ce formulaire (suppression d'un commentaire, changement d'un courriel, etc.) ne sont pas appliquées aux abonnements aux notifications. Pour ce faire, utiliser plutôt le formulaire réservé aux abonnements.") . "</p>\n";
 				
 				$contenuFormulaire .= '<p>' . T_("Aussi, la liste des commentaires est triable. Pour ce faire, cliquer sur la flèche correspondant au commentaire à déplacer et glisser-la à l'endroit désiré à l'intérieur de la liste.") . "</p>\n";
 				$contenuFormulaire .= "</div><!-- /.bDcorps -->\n";
