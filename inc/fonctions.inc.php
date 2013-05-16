bad3df46917a46da2b7262941f48473e94b2e624
Jean-Philippe Fleury <contact@jpfleury.net>
2013-05-16 19:30:26 -0400

    Utilisation de `preg_replace_callback()` au lieu de `preg_replace()` et du modificateur `e`
    `preg_replace()` échappait certains caractères dans la chaîne de remplacement,
    et le modificateur `e` est obsolète à partir de PHP 5.5.0.

--- inc/fonctions.inc.php
+++ inc/fonctions.inc.php
@@ -296,7 +296,7 @@ function apercuDansCategorie($racine, $urlRacine, $infosPage, $adresse, $baliseT
 	}
 	
 	// Incrémentation des niveaux de titre 2 à 5 (par exemple, `<h2 class="classe">Sous-titre</h2>` devient `<h3 class="classe">Sous-titre</h3>`). Le but est d'éviter que des sous-titres affichés dans l'aperçu aient le même niveau (2) que le titre de l'aperçu lui-même. Cela découle du fait que le titre d'une page est habituellement de niveau 1 alors que dans l'aperçu, le même titre est de niveau 2.
-	$texteApercu = preg_replace('#<h([2-5])(.*?>.*?</h)\1\b#e', '"<h" . ("$1" + 1) . "$2" . ("$1" + 1)', $texteApercu);
+	$texteApercu = preg_replace_callback('#<h([2-5])(.*?>.*?</h)\1\b#', create_function('$elements', '$niveauTitre = $elements[1] + 1; return "<h" . $niveauTitre . $elements[2] . $niveauTitre;'), $texteApercu);
 	
 	$apercu .= $texteApercu . "\n";
 	$apercu .= "</div><!-- /.descriptionApercu -->\n";
